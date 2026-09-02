93df9e3c087eb4b6cec13a0eae665f70bfad61c3
samuelburnham <45365069+samuelburnham@users.noreply.github.com>
2026-09-02 15:50:04 -0400

    Make the Lean template self-contained

--- Main.lean
+++ Main.lean
@@ -1,20 +1,4 @@
 import Template
-import Ix.Benchmark.Bench
 
-def add (input: Nat): IO Nat := do
-  pure $ input + 1
-
-def addBench := bgroup "Add" [
-  benchIO "add 1" add 1,
-  benchIO "add 10" add 10
-] { report := false }
-
-def addBench' := bgroup "Add'" [
-  benchIO "add' 1" add 1,
-  benchIO "add' 10" add 10
-]
-
-def main : IO Unit := do
-  --IO.println "hello"
-  let _result ← addBench
-  let _result ← addBench'
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
--- lakefile.lean
+++ lakefile.lean
@@ -1,25 +1,11 @@
 import Lake
 open Lake DSL
 
-require ix from git "https://github.com/argumentcomputer/ix" @ "fa2335d35cd6f71ef3d21fb67ce3d1501e9f45c5"
-
 package "template" where
   version := v!"0.1.0"
 
-lean_lib «Template» where
-  -- add library configuration options here
+lean_lib «Template»
 
 @[default_target]
 lean_exe "template" where
   root := `Main
-
-lean_exe "bench-test" where
-  root := `Benchmarks.Main
-
-script "get-exe-targets" := do
-  let pkg ← getRootPackage
-  let exeTargets := pkg.configTargets LeanExe.configKind
-  for tgt in exeTargets do
-    IO.println <| tgt.name.toString |>.stripPrefix "«" |>.stripSuffix "»"
-  return 0
-
